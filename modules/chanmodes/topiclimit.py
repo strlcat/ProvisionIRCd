40e6d1a889a8c78b317308bfe50184a0881db84a
Andrey Rys <rys@lynxlynx.ru>
2024-07-18 22:32:22 +0200

    +t: step up to chanop

--- modules/chanmodes/topiclimit.py
+++ modules/chanmodes/topiclimit.py
@@ -9,6 +9,6 @@ def init(module):
     Chmode_t = Channelmode()
     Chmode_t.flag = 't'
     Chmode_t.paramcount = 0
-    Chmode_t.is_ok = Channelmode.allow_halfop
+    Chmode_t.is_ok = Channelmode.allow_chanop
     Chmode_t.desc = "Topic cannot be changed from outside"
     Channelmode.add(module, Chmode_t)
