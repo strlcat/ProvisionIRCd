4a3887f3eade043d58458646e5c9030c67139f2f
Andrey Rys <rys@lynxlynx.ru>
2024-07-28 02:00:07 +0200

    /USERHOST: always return cloakhost, dont expose realhost of opers

--- modules/m_ison.py
+++ modules/m_ison.py
@@ -27,7 +27,7 @@ def cmd_userhost(client, recv):
     hosts = []
     for nick in recv[1:]:
         for u_client in [u_client for u_client in IRCD.global_users() if u_client.name.lower() == nick.lower() and u_client.name not in hosts]:
-            h = f"{u_client.name}*=+{u_client.user.username}@{u_client.user.cloakhost if 'o' not in u_client.user.modes else u_client.user.realhost}"
+            h = f"{u_client.name}*=+{u_client.user.username}@{u_client.user.cloakhost}"
             if h not in hosts:
                 hosts.append(h)
     client.sendnumeric(Numeric.RPL_USERHOST, ' '.join(hosts))
