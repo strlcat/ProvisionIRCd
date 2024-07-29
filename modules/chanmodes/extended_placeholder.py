81a5310f6a4b63d6e0947368d7ba2e650943259f
Andrey Rys <rys@lynxlynx.ru>
2024-07-29 16:32:45 +0200

    +X: introduce concept of extended modes to IRC (not implemented yet)

---
+++ modules/chanmodes/extended_placeholder.py
@@ -0,0 +1,26 @@
+"""
+channel mode +X (eXtended modes)
+Currently a placeholder, not available to anyone.
+Format: +X <extspec> <extparam>
+"""
+
+from handle.core import Channelmode
+
+def conv_param_X(param):
+	return param
+
+def sjoin_check_X(a, b):
+	return 1
+
+def init(module):
+	Cmode_X = Channelmode()
+	Cmode_X.flag = "X"
+	Cmode_X.paramcount = 2
+	Cmode_X.is_ok = Channelmode.allow_none
+	Cmode_X.conv_param = conv_param_X
+	Cmode_X.sjoin_check = sjoin_check_X
+	Cmode_X.level = 7
+	Cmode_X.unset_with_param = 1
+	Cmode_X.param_help = "<extspec> <extparam>"
+	Cmode_X.desc = "Set extended arbitrary syntax channel modes. See /HELP XMODES (not implemented yet)"
+	Channelmode.add(module, Cmode_X)
