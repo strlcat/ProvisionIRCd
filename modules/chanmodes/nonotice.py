ecf3e39b8e086c30d755d592e1f0b1beab8ac617
Andrey Rys <rys@lynxlynx.ru>
2024-08-18 19:37:18 +0200

    chmode +T rename to +D

--- modules/chanmodes/nonotice.py
+++ modules/chanmodes/nonotice.py
@@ -1,12 +1,12 @@
 """
-channel mode +T (no notices in the channel)
+channel mode +D (no notices in the channel)
 """
 
 from handle.core import Channelmode, Hook, Numeric
 
 
 def can_channel_notice(client, channel, message, sendtype):
-	if 'T' not in channel.modes or sendtype != "NOTICE":
+	if 'D' not in channel.modes or sendtype != "NOTICE":
 		return Hook.ALLOW
 
 	if not client.user or channel.client_has_membermodes(client, "hoaq") or client.has_permission("channel:override:message:notice"):
@@ -17,9 +17,9 @@ def can_channel_notice(client, channel, message, sendtype):
 
 
 def init(module):
-	Cmode_T = Channelmode()
-	Cmode_T.flag = 'T'
-	Cmode_T.is_ok = Channelmode.allow_halfop
-	Cmode_T.desc = "Notices are not allowed in the channel"
-	Channelmode.add(module, Cmode_T)
+	Cmode_D = Channelmode()
+	Cmode_D.flag = 'D'
+	Cmode_D.is_ok = Channelmode.allow_halfop
+	Cmode_D.desc = "Notices are not allowed in the channel"
+	Channelmode.add(module, Cmode_D)
 	Hook.add(Hook.CAN_SEND_TO_CHANNEL, can_channel_notice)
