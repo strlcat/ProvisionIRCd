d6e49905678c9c972ff066e49f11496dc2effc6e
Andrey Rys <rys@strlcat.eu>
2025-03-03 22:47:29 +0100

    03032025b: changes to the source
    chmode +RZ: simplify, add set check
    umode +q: fix description
    remove text extban

--- modules/usermodes/coremodes.py
+++ modules/usermodes/coremodes.py
@@ -45,7 +45,7 @@ def init(module):
 	Usermode.add(module, 'F', 1, 0, Usermode.allow_all, "Prevent channel redirection (+F)")
 	Usermode.add(module, 'o', 1, 1, Usermode.allow_opers, "Marks the user as an IRC Operator")
 	Usermode.add(module, 'p', 0, 0, umode_p_isok, "Immutable to ping timeouts [Settable by services]")
-	Usermode.add(module, 'q', 1, 1, umode_q_isok, "Protected on all channels")
+	Usermode.add(module, 'q', 1, 1, umode_q_isok, "Protected on all channels [IRCops only]")
 	Usermode.add(module, 'r', 1, 0, Usermode.allow_services, "Identifies the nick as being logged in")
 	Usermode.add(module, 's', 1, 1, Usermode.allow_opers, "Can receive server notices")
 	Usermode.add(module, "x", 1, 0, umode_x_isok, "Hides real host with cloaked host")
--- modules/usermodes/m_blockmsg.py
+++ modules/usermodes/m_blockmsg.py
@@ -4,6 +4,24 @@ provides usermodes +R, +Z, +D and +O to block private messages
 
 from handle.core import Usermode, Numeric, Hook
 
+def umode_R_isok(client):
+	if Usermode.allow_services(client):
+		return 1
+	if 'o' in client.user.modes:
+		return 1
+	if 'r' in client.user.modes:
+		return 1
+	return 0
+
+def umode_Z_isok(client):
+	if Usermode.allow_services(client):
+		return 1
+	if 'o' in client.user.modes:
+		return 1
+	if 'z' in client.user.modes:
+		return 1
+	return 0
+
 def blockmsg_to_user(client, to_client, msg, sendtype):
 	if 'D' in to_client.user.modes:
 		client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "This user does not accept private messages")
@@ -14,52 +32,21 @@ def blockmsg_to_user(client, to_client, msg, sendtype):
 			client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "You need to be an IRC Operator to talk privately to this user")
 			return Hook.DENY
 
-	if 'O' in client.user.modes:
-		if 'o' not in to_client.user.modes:
-			client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "Message not sent. You have usermode +O but recipient is not an IRC Operator")
-			return Hook.DENY
-
 	if 'R' in to_client.user.modes:
 		if 'r' not in client.user.modes:
 			client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "You need a registered nickname to talk privately to this user")
-			return Hook.DENY
-		if 'r' not in to_client.user.modes:
-			client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "Message not sent. Recipient has usermode +R but is not using a registered nickname")
-			return Hook.DENY
-
-	if 'R' in client.user.modes:
-		if 'S' in to_client.user.modes and 'o' in to_client.user.modes:
-			return msg
-		if 'r' not in client.user.modes:
-			client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "Message not sent. You have usermode +R but are not using a registered nickname")
-			return Hook.DENY
-		if 'r' not in to_client.user.modes:
-			client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "Message not sent. You have usermode +R but recipient is not using a registered nickname")
 			return Hook.DENY
 
 	if 'Z' in to_client.user.modes:
 		if 'z' not in client.user.modes:
 			client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "You need to be on a secure connection to talk privately to this user")
 			return Hook.DENY
-		if 'z' not in to_client.user.modes:
-			client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "Message not sent. Recipient has usermode +Z but is not using a secure connection")
-			return Hook.DENY
-
-	if 'Z' in client.user.modes:
-		if 'S' in to_client.user.modes and 'o' in to_client.user.modes:
-			return msg
-		if 'z' not in client.user.modes:
-			client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "Message not sent. You have usermode +Z but are not using a secure connection")
-			return Hook.DENY
-		if 'z' not in to_client.user.modes:
-			client.sendnumeric(Numeric.ERR_CANTSENDTOUSER, to_client.name, "Message not sent. You have usermode +Z but recipient is not using a secure connection")
-			return Hook.DENY
 
 	return msg
 
 def init(module):
-	Usermode.add(module, "R", 1, 0, Usermode.allow_all, "Only users with a registered nickname can private message you")
+	Usermode.add(module, "R", 1, 0, umode_R_isok, "Only users with a registered nickname can private message you")
 	Usermode.add(module, "O", 1, 1, Usermode.allow_opers, "Only IRC Operators can private message you")
-	Usermode.add(module, "Z", 1, 0, Usermode.allow_all, "Only users on a secure connection can private message you")
+	Usermode.add(module, "Z", 1, 0, umode_Z_isok, "Only users on a secure connection can private message you")
 	Usermode.add(module, "D", 1, 0, Usermode.allow_all, "No-one can private message you")
 	Hook.add(Hook.CAN_SEND_TO_USER, blockmsg_to_user)
